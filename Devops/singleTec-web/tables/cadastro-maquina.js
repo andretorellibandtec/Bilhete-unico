1730167c100c403bf5fc5c53d9018e9eada86a2b
brunoalmeidabandtec <miguel.moreira@nbandtec.com.br>
2020-06-09 14:50:07 -0300

    atualizando a table de maquina

---
+++ Devops/singleTec-web/tables/cadastro-maquina.js
@@ -0,0 +1,26 @@
+const { sequelize, Sequelize } = require("../conexao/conexao")
+
+let modelEmpresa = sequelize.define('Endereco_Maquina', {
+    idEnderecoMaquina: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    Estacao: {
+      type: Sequelize.STRING
+    },
+    Terminal: {
+      type: Sequelize.STRING
+    },
+    DataHora: {
+      type: Sequelize.DATE
+    },
+    
+     
+   
+  }, { tableName: 'Endereco_Maquina' });
+  
+  module.exports = modelEndereco_Maquina;
+
+
+
+
