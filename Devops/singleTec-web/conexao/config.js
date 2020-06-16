54f23ddc79bd39b3f0455face931be4994af6ee5
viniciussobral <vinicius.araujo@bandtec.com.br>
2020-06-15 21:07:16 -0300

    conexão com o banco

--- Devops/singleTec-web/conexao/config.js
+++ Devops/singleTec-web/conexao/config.js
@@ -1,9 +1,40 @@
+// module.exports = {
+//   development: {
+//     username: 'localadmin@svrsingletec',
+//     password: '#Gfgrupo3b',
+//     database: 'bdsingletec',
+//     host: 'svrsingletec.database.windows.net',
+//     dialect: 'mssql'
+//   }
+// }
+
 module.exports = {
   development: {
-    username: 'root',
-    password: 'root',
-    database: 'totens',
-    host: 'localhost',
-    dialect: 'mysql'
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'localadmin@svrsingletec',  
+    password: '#Gfgrupo3b',
+    database: 'bdsingletec',
+    host: 'svrsingletec.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    }, 
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
   }
-}
+};
