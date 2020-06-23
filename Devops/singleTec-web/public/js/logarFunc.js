13c23140de7f5cf09d7468fa98280ba87c3cb2d5
viniciussobral <vinicius.araujo@bandtec.com.br>
2020-06-22 23:34:53 -0300

    Atualizacao02

--- Devops/singleTec-web/public/js/logarFunc.js
+++ Devops/singleTec-web/public/js/logarFunc.js
@@ -1,22 +1,12 @@
 let form = document.getElementById("form_login_func");
 let input_email = document.getElementById("email");
 let input_senha = document.getElementById("senha");
-// backend que pega valores do front// exemplo valor que user vai digitar e passan dentro  de uma variavel chamada input
-
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
     let resposta = await axios.post("/funcionario/logarfuncionario", {
-<<<<<<< HEAD
         email: input_email.value,
         senha: input_senha.value
-=======
-
-        email: input_email.value,
-        senha: input_senha.value
-
-        
->>>>>>> 90f2bd77c40e6171c20915189f70929a6158ed60
 
     });
 
